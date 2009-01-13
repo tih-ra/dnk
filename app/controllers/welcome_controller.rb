33195b9a30a808d53f4e6227410815faad93ed15
Andriy Bazyuta <tihhra@gmail.com>
2009-01-13 13:30:36 +0200

    f

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    @blogs = Blog.paginate(:page => params[:page], :per_page=>5, :order => 'created_at DESC')
+    @blogs = Blog.all(:order => 'created_at DESC', :limit=>3)
+    @blogs.concat(Portfolio.all(:order => 'created_at DESC', :limit=>3))
+    #Blog.paginate(:page => params[:page], :per_page=>5, :order => 'created_at DESC')
     @bigbord = Bigbord.last
     respond_to do |format|
       format.html # index.html.erb
